6fd6e31d35128d99c38f38a5621f1bd52c2774c3
Shrikaran <52494360+Shrikaran-Kanagaraj@users.noreply.github.com>
2021-11-30 10:37:18 +0530

    Add files via upload

---
+++ Src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import FormReact from './components/FormReact';
+
+function App() {
+  return (
+    <div className="App">
+      <FormReact/>
+    </div>
+  );
+}
+ 
+export default App;
